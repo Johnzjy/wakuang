e620bc5b28bc88c59fd3e643073e05719f18a3e3
Zhang <johnson.JY.zhang@philips.com>
2018-06-11 16:43:39 +0800

    加入一些基本数据，期望FA成为基础库

--- data_contorl/FundamentalAnalysis.py
+++ data_contorl/FundamentalAnalysis.py
@@ -20,6 +20,7 @@ class FudAnalysis(object):
         self._date = str(self.today)
         self.base_datas = ts.get_stock_basics()
 
+
     @property
     def date(self):
         return self._date
@@ -27,7 +28,7 @@ class FudAnalysis(object):
     @property
     def year(self):
         return self._date[0:4]
-
+#TODO: 季度数据目前还没有增加
     @property
     def quarter(self):
         return '2'
@@ -56,6 +57,12 @@ class FudAnalysis(object):
         return _totals
 
     @property
+    def outstanding(self):
+        _out = list(self.search_stock_basics().outstanding.values)
+        _out = _out + ['亿股']
+        return _out
+
+    @property
     def pb(self):  # 市净率
         return self.search_stock_basics().pb.values[0]
 
@@ -63,11 +70,40 @@ class FudAnalysis(object):
     def pe(self):  # 市盈率
         return self.search_stock_basics().pe.values[0]
     @property
-    def bvps(self):
+    def bvps(self):#每股净资产
         return self.search_stock_basics().bvps.values[0]
     @property
     def esp(self):  # 每股收益
         return self.search_stock_basics().esp.values[0]
+    
+    @property
+    def open(self):#当日开盘价格
+        data=self.get_now_price().open.values[0]
+        return float(data)
+    @property
+    def high(self):#当日最高点
+        data=self.get_now_price().high.values[0]
+        return float(data)
+    
+    @property
+    def low(self): #当日最低点
+        data=self.get_now_price().low.values[0]
+        return float(data)
+    @property
+    def price(self): #当前价格
+        data=self.get_now_price().price.values[0]
+        return float(data)
+    @property
+    def vol(self): #当前交易量
+        data=self.get_now_price().volume.values[0]
+        return float(data)
+    @property
+    def amount(self): #当前交易金额
+        data=self.get_now_price().amount.values[0]
+        return float(data)
+    @property
+    def turnover(self):
+        pass
 
     def search_stock_basics(self):  # 搜寻股票在基本数据
         df = self.base_datas.reset_index()
@@ -75,7 +111,9 @@ class FudAnalysis(object):
             return df[df['code'] == self.code]
         else:
             pass
-
+    def get_now_price(self):
+        df=ts.get_realtime_quotes(self.code)
+        return df
     def check_code(self):
         if len(self.code) == 6:
             return True
@@ -85,7 +123,7 @@ class FudAnalysis(object):
 
 #TODO: 业绩预告因为发布时间不同，所有系统所存的文件未必是最新的，所哟定期需要更新。目前不提供自动更新
 
-    def check_forecast(self):
+    def check_forecast(self,upgrade=False):
         """
         检查该季度内是否发布业绩预告，放在report里面
         Parameters
@@ -104,7 +142,7 @@ class FudAnalysis(object):
         range:
         """
         _path = '../report/forecast_%s-%s.csv' % (self.year, self.quarter)
-        if os.path.exists(_path):
+        if os.path.exists(_path) and (upgrade == False):
             all_forecast = pandas.read_csv(_path, encoding='gbk', header=0)
 
             all_forecast.code = all_forecast.code.map(
@@ -162,7 +200,7 @@ class FudAnalysis(object):
 
     def check_rs(self, durting=None):
         """
-        检查stock 是否在这里面
+        检查stock 是否在这里面解禁股中
 
         Parameters
         ----------
@@ -189,6 +227,8 @@ class FudAnalysis(object):
             return None
 
 if __name__ == "__main__":
-    x = FudAnalysis('600027')
-    df = 1
-    print(x.name, x.code)
+    x = FudAnalysis('600362')
+    df = x.get_now_price()
+    z=ts.get_czce_daily()
+    print(x.name, x.code,)
+    print(x.base_datas)
